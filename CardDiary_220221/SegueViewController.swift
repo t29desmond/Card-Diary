cbf9db2ead5fdf625164f465df7547740ec65a94
t29desmond <t29desmond@gmail.com>
2022-02-25 01:13:48 +0900

    ### 히스토리 뷰 생성하기 - 4 [히스토리 화면] 디자인 가이드에 맞춰 AutoLayout 적용 학습 - AutoLayout -> 오류 해결 - Button 제거 -> 미해결

---
+++ CardDiary_220221/SegueViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SegueViewController.swift
+//  CardDiary_220221
+//
+//  Created by 윤대식 on 2022/02/24.
+//
+
+import UIKit
+
+class SegueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
