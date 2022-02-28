2f3474f4d611296466ff39d866a8a89ae8f966d3
윤대식 <t29desmond@gmail.com>
2022-02-28 14:34:51 +0900

    ### CustomView 적용 [메인/히스토리 화면] CustomView - 공통 컨포넌트 (날짜/요일 ) CustomView 적용

---
+++ CardDiary_220221/CustomView_main.swift
@@ -0,0 +1,44 @@
+//
+//  CustomView.swift
+//  CardDiary_220221
+//
+//  Created by 윤대식 on 2022/02/28.
+//
+import Foundation
+import UIKit
+
+@IBDesignable
+
+class CustomView: UIView {
+
+    override init(frame: CGRect) {
+         super.init(frame: frame)
+         xibSetup()
+     }
+
+     override func prepareForInterfaceBuilder() {
+         super.prepareForInterfaceBuilder()
+          xibSetup()
+     }
+
+     required init?(coder aDecoder: NSCoder) {
+         super.init(coder: aDecoder)
+         xibSetup()
+     }
+
+     func xibSetup() {
+         guard let view = loadViewFromNib(nib: "CustomView_main") else {
+             return
+         }
+         view.frame = bounds
+         view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+         addSubview(view)
+     }
+
+     func loadViewFromNib(nib: String) -> UIView? {
+         let bundle = Bundle(for: type(of: self))
+         let nib = UINib(nibName: nib, bundle: bundle)
+         return nib.instantiate(withOwner: self, options: nil).first as? UIView
+     }
+ }
+
